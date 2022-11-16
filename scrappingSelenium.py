2d09bc3a59f18d7583ec0e0774f33e0b896404e3
QueenAnnesRevenge <nicolas.ruau27@gmail.com>
2022-11-16 14:09:52 +0100

    title, author, description and links

--- scrappingSelenium.py
+++ scrappingSelenium.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 import packaging
+import re 
 
 s=Service(ChromeDriverManager().install())
 options = Options()
@@ -12,7 +13,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=s, chrome_options=options)
 
-url = "https://www.youtube.com/watch?v=lDOswsnwYPk"
+url = "https://www.youtube.com/watch?v=V-D6eZwMd5g"
 driver.get(url)
 time.sleep(2)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -29,15 +30,25 @@ print("The author is", author)
 print("----")
 
 ## pocebleu (views pr l'instant)
+##raw_like = soup.find('button', {'class': 'yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading yt-spec-button-shape-next--segmented-start'})
 #likes = soup.find("span", {"class" : "like-button-renderer"}).text
 #thumbs = soup.find("span", {"class": "style-scope yt-formatted-string bold"})
 #print("this video has been liked" ,likes,"times")
-#print("----")
+##print(raw_like["aria-label"])
+
+
+##.find_all(re.compile("^[1-9](,[1-9])*$"))
+
+##print("----")
 
 ## description
-description = soup.find("div", {"id" : "snippet"}).text
-print(description)
+pattern = re.compile('(?<=shortDescription":").*(?=","isCrawlable)')
+description = pattern.findall(str(soup))[0].replace('\n','\n')
+print("Description: "+description)
 print("----")
 
+## links in Description
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', description)
+print("Links in description : ", urls, "\n")
 
 driver.quit()

39e95605db865d9664d776ac6ee72a32ecda0e7c
QueenAnnesRevenge <nicolas.ruau27@gmail.com>
2022-11-16 14:50:57 +0100

    pareil

--- scrappingSelenium.py
+++ scrappingSelenium.py
@@ -13,33 +13,31 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=s, chrome_options=options)
 
-url = "https://www.youtube.com/watch?v=V-D6eZwMd5g"
+url = "https://www.youtube.com/watch?v=LPpqcvIlrhQ"
 driver.get(url)
 time.sleep(2)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 ## title
 title = soup.find("meta", {"name" : "title"})["content"]
-print("----")
-print("The title is", title)
+print("###")
+print("Title:", title)
 print("----")
 
 ## author 
 author = soup.find("link", {"itemprop" : "name"})["content"]
-print("The author is", author)
+print("Author:", author)
 print("----")
 
 ## pocebleu (views pr l'instant)
-##raw_like = soup.find('button', {'class': 'yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading yt-spec-button-shape-next--segmented-start'})
-#likes = soup.find("span", {"class" : "like-button-renderer"}).text
-#thumbs = soup.find("span", {"class": "style-scope yt-formatted-string bold"})
-#print("this video has been liked" ,likes,"times")
-##print(raw_like["aria-label"])
+raw_like = soup.find('button', {'class': 'yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading yt-spec-button-shape-next--segmented-start'})
+#soup.find("span", {"class" : "like-button-renderer"}).text
+print(raw_like["aria-label"])
 
 
 ##.find_all(re.compile("^[1-9](,[1-9])*$"))
 
-##print("----")
+print("----")
 
 ## description
 pattern = re.compile('(?<=shortDescription":").*(?=","isCrawlable)')
@@ -49,6 +47,13 @@ print("----")
 
 ## links in Description
 urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', description)
-print("Links in description : ", urls, "\n")
+print("Links in description : ", urls)
+print("----")
+
+## id video 
+id = soup.find("meta", {"itemprop" : "videoId"})
+print("ID:",id["content"])
+
+print('###')
 
 driver.quit()
